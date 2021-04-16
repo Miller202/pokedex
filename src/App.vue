687187ae058f2451de3505c142c68491a0e7124b
Michael Miller <mullerg4michael@gmail.com>
2021-04-16 10:06:03 -0300

    HTTP Requisition and pokemons filter;

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
+  data () {
+    return {
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log("lista de pokemons");
+      this.pokemons = res.data.results;
+    })
+  },
   components: {
-    HelloWorld
+    Pokemon
   }
 }
 </script>
